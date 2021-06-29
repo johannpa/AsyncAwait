bcc99aeb5484798a277c0de26999d7f117f2333d
johannpa <johann_paque@hotmail.fr>
2021-06-29 11:34:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+
+            string url = "https:/...";
+
+            Console.Write("Téléchargement...");
+            var task = httpClient.GetStringAsync(url);
+            task.Wait();
+
+            Console.WriteLine("OK");
+
+            Console.WriteLine(task.Result);
+        }
+
+        
+    }
+}
